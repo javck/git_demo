73ab1926bb096628eb849e7c925f3bc18a0b00aa
品爵老師 <javck.zack@gmail.com>
2022-07-25 13:23:35 +0800

    testing2

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -21,15 +21,14 @@ class SiteController extends Controller
     {
         return 'Yolo';
     }
-<<<<<<< HEAD
 
-    public functoin yolo2()
+    public function yolo2()
     {
+    
+    }
         
-=======
     public function spcpcps()
     {
         return '1545451245154148514515487514851414514512541412';
->>>>>>> 1f45e3d9e70604f3b9fed6c3751fac06ae538f56
     }
 }
