6993b493be8570e14f0169311e6520d722c603f3
spencer\spc <rtggjkdsgn@gmail.com>
2022-07-25 13:58:08 +0800

    TEST

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -37,32 +37,19 @@ class SiteController extends Controller
         return '1545451245154148514515487514851414514512541412';
     }
 
-
     public function kelly()
     {
         return 'I am Kelly :)';
 
-    public function yolo3()
-    {
-        return 'yolo3';
-    }
+}
     public function yolo4()
     {
         return 'yolo3';
 
     }
-}
-
-    }
-
 
     public function yolo5()
     {
         return 'yolo3';
+    }
 }
-
-    public function kelly()
-    {
-        return 'I am Kelly :)';
-
-    }}
