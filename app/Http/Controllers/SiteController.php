4deff8992c4057377176b328e7cf17177dfb9217
品爵老師 <javck.zack@gmail.com>
2022-07-25 14:52:06 +0800

    Bugs Gone

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -69,6 +69,9 @@ class SiteController extends Controller
         return 'devDone';
     }
 
-    public fkdjfkdfkfkdfjf
+    public function bugDone()
+    {
+        return 'bugs gone';
+    }
 
 }
